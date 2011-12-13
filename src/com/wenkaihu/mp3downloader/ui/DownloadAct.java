5ebbb13f3bc8c4a326695ca647c971b6570bfd23
wenkaihu <wenkaihu@gmail.com>
2011-12-13 20:13:10 +0800

    download page initial

--- src/com/wenkaihu/mp3downloader/ui/DownloadAct.java
+++ src/com/wenkaihu/mp3downloader/ui/DownloadAct.java
@@ -1,9 +1,9 @@
 package com.wenkaihu.mp3downloader.ui;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class DownloadAct extends Activity {
+public class DownloadAct extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
